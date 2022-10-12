06568dd5700649b0fe95e6dc1332672c8edf0745
habbkad <habibkadiri52@gmail.com>
2022-10-12 14:11:19 +0000

    Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header2 from "./Componets/Header";
+import NavBar from "./Componets/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Send</button>
+      <Header2 />
+      <NavBar />
+      <NewComponent />
     </div>
+  );
+}
+
+function NewComponent() {
+  return (
+    <>
+      <h1>Codetrain is awesome!!!</h1>
+    </>
   );
 }
 
---
+++ src/Componets/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../logo512.png";
+
+function NavBar() {
+  return (
+    <>
+      <h1>Sam generals</h1>
+      <img src={logo} />
+    </>
+  );
+}
+
+export default NavBar;
