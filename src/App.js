af346c333d0e4dbe9d5d21f56a2296ee0a27813a
habbkad <habibkadiri52@gmail.com>
2022-11-16 14:49:52 +0000

    Done with Update And Delete

--- src/App.js
+++ src/App.js
@@ -10,18 +10,39 @@ class App extends Component {
 
     this.state = {
       students: [
-        { name: "Sarah", gen: 23 },
-        { name: "Elkanah", gen: 23 },
-        { name: "Joshua", gen: 23 },
-        { name: "Sam", gen: 23 },
-        { name: "Esther", gen: 23 },
+        { name: "Sarah", gen: 23, id: "svdjvsdiugiuiiyiy309" },
+        { name: "Elkanah", gen: 23, id: "kjerhohhithoh89y58ndfn" },
+        { name: "Joshua", gen: 23, id: "jbjdbiuehi985498u89kn" },
+        { name: "Sam", gen: 23, id: "jgdfs72tt74g98y20idl" },
+        { name: "Esther", gen: 23, id: "jhsgduyw78t483t834dn" },
       ],
     };
   }
 
+  deleteStudent = (student_id) => {
+    let newStudents = this.state.students.filter(
+      (student) => student_id !== student.id
+    );
+    this.setState({
+      students: newStudents,
+    });
+  };
+
   addNewStudent = (newStudent) => {
     this.setState({
       students: [...this.state.students, newStudent],
+    });
+  };
+
+  editStudent = (student_id, newDetails) => {
+    this.setState({
+      students: this.state.students.map((student) => {
+        if (student_id == student.id) {
+          return newDetails;
+        } else {
+          return student;
+        }
+      }),
     });
   };
   render() {
@@ -33,7 +54,10 @@ class App extends Component {
               <Form addStudent={this.addNewStudent} />
             </Col>
             <Col md={6}>
-              <Users students={this.state.students} />
+              <Users
+                students={this.state.students}
+                editUser={this.editStudent}
+              />
             </Col>
           </Row>
         </Container>
